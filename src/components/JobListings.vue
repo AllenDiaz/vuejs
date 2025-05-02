098f2582840e1e63b9e5b58c211d7e2302804ab2
alled <allengdiaz2019@gmail.com>
2025-05-02 13:15:11 +0800

    Done adding two routes and configure it

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -2,7 +2,7 @@
 import JobListing from '@/components/JobListing.vue';
 import jobData from '@/jobs.json';
 import { ref, defineProps } from 'vue';
-
+ 
 defineProps({
     limit: Number,
     showButton: {
@@ -15,7 +15,7 @@ const jobs = ref(jobData);
 </script>
 <template>
 
-    <section v-if="showButton" class="bg-blue-50 px-4 py-10 ">
+    <section class="bg-blue-50 px-4 py-10 ">
         <div class="container-xl lg:container m-auto">
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
                 Browse Jobs
@@ -25,7 +25,7 @@ const jobs = ref(jobData);
             </div>
         </div>
     </section>
-    <section class="m-auto max-w-lg my-10 px-6">
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
       <a
         href="jobs.html"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
+import Hero from '@/components/Hero.vue';
+</script>
+
+<template>
+  <Hero title="Test Title" subtitle="Test Subtitle"/>
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true"/>
+</template>
